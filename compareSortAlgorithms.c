531ad87b3463c62b18457a4e81efd527e5c88970
rigelcodes <118703524+rigelcodes@users.noreply.github.com>
2023-03-26 22:29:31 -0400

    Update compareSortAlgorithms.c
    Update: Properly added a way to keep up with the extra memory used in the merge function.

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -1,6 +1,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int extraMemoryAllocated;
 
@@ -12,9 +13,11 @@ void merge(int pData[], int l, int m, int r)
 	int n1 = m - l + 1;
 	int n2 = r - m;
 
+	// allocate memory for the left and right side
 	int *L = (int*) malloc(n1*sizeof(int));
 	int *R = (int*) malloc(n2*sizeof(int));
 
+	// traverse through n1 and n2
 	for(i = 0; i < n1; i++)
 	{
 		L[i] = pData[l + i];
@@ -24,9 +27,11 @@ void merge(int pData[], int l, int m, int r)
 		R[j] = pData[m + 1 + j];
 	}
 
+	// set variables equal to 0
 	i = 0;
 	j = 0;
 	k = l;
+
 	while(i < n1 && j < n2)
 	{
 		if(L[i] <= R[j])
@@ -56,18 +61,22 @@ void merge(int pData[], int l, int m, int r)
 	free(L);
 	free(R);
 }
+
 void mergeSort(int pData[], int l, int r)
 {
-if (l < r)
+	if (l < r)
 	{
 		int m  = (l + r) / 2;
 
+		// send it through recursively
 		mergeSort(pData, l, m);
 		mergeSort(pData, m + 1, r);
 
 		merge(pData, l, m, r);
+
+		// allocated memory updated for merge function
+		extraMemoryAllocated += (r - l + 1) * sizeof(int) / 4;
 	}
-	extraMemoryAllocated++;
 }
 
 // implement insertion sort
@@ -75,13 +84,18 @@ if (l < r)
 void insertionSort(int* pData, int n)
 {
 	int x, k, element;
+
+	// traverse through loop
 	for(x = 1; x < n; x++)
 	{
+		// set element equal to that of pData in this loop
 		element = pData[x];
+
 		for(k = x - 1; k >= 0; k--)
 		{
 			if(pData[k] > element)
 			{
+				// update
 				pData[k + 1] = pData[k];
 			}
 			else
@@ -103,6 +117,7 @@ void bubbleSort(int* pData, int n)
 	{
 		for(x = 0; x < n - i - 1; x++)
 		{
+			// after traversing, swap temp variables
 			if(pData[x] > pData[x + 1])
 			{
 				tempVar = pData[x];
@@ -118,13 +133,17 @@ void bubbleSort(int* pData, int n)
 void selectionSort(int* pData, int n)
 {
 	int r, y, min_index, tempVar;
+
+	// traverse through one less than the original value in n
 	for(r = 0; r < n - 1; r++)
 	{
 		min_index = r;
+
 		for(y = r + 1; y < n; y++)
 		{
 			if(pData[y] < pData[min_index])
 			{
+				// update minimum index value
 				min_index = y;
 			}
 		}
@@ -145,7 +164,6 @@ int parseData(char *inputFileName, int **ppData)
 	{
 		fscanf(inFile,"%d\n",&dataSz);
 		*ppData = (int *)malloc(sizeof(int) * dataSz);
-		// Implement parse data block
 		if(*ppData)
 		{
 			for(int i = 0; i < dataSz; ++i)
@@ -153,6 +171,7 @@ int parseData(char *inputFileName, int **ppData)
 				fscanf(inFile, "%d\n", &((*ppData)[i]));
 			}
 		}
+		// Implement parse data block
 	}
 	fclose(inFile);
 	return dataSz;
